a0971d6f0230485c81b1c5985da2403b321c1ecb
Liton Islam <nurulislamliton24@gmail.com>
2025-01-19 23:48:58 +0600

    project 20% done

--- first-project/script.js
+++ first-project/script.js
@@ -3,6 +3,7 @@
 const secretNummber = Math.trunc(Math.random()*20) + 1;
 document.querySelector('.number').textContent = secretNummber;
 
+let score= 20;
 document.querySelector('.check').addEventListener('click',function(){
 
    const guess= Number( document.querySelector('.guess').value);
@@ -12,9 +13,24 @@ document.querySelector('.check').addEventListener('click',function(){
    }else if(guess === secretNummber ){
     document.querySelector('.message').textContent = 'Correct Number!';
    }else if(guess > secretNummber ){
-    document.querySelector('.message').textContent = 'big Number!';
+      if(score > 1){
+         document.querySelector('.message').textContent = 'big Number!';
+         score--;
+         document.querySelector('.score').textContent = score;
+      }else{
+         document.querySelector('.message').textContent = 'Your lost the game';
+         document.querySelector('.score').textContent = 0;
+      }
+  
    } else if(guess < secretNummber ){
-    document.querySelector('.message').textContent = 'small Number!';
+      if(score > 1){
+      document.querySelector('.message').textContent = 'small Number!';
+      score--;
+      document.querySelector('.score').textContent = score;
+      }else{
+         document.querySelector('.message').textContent = 'Your lost the game';
+         document.querySelector('.score').textContent = 0;
+      }
    }else{
     document.querySelector('.message').textContent = 'tray again';
    }
