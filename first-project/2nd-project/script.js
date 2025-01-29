f16533c9f48854f7b66ba3eebf0e3f7efc7100b8
Liton Islam <litonislam.nitex@gmail.com>
2025-01-29 17:58:28 +0600

    2nd project add

---
+++ first-project/2nd-project/script.js
@@ -0,0 +1,5 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overly');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
