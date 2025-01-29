938e84fcacb83e3d139a46a5f3b692d4b676ba43
Liton Islam <nurulislamliton24@gmail.com>
2025-01-29 23:11:24 +0600

    classlist add

--- first-project/2nd-project/script.js
+++ first-project/2nd-project/script.js
@@ -1,5 +1,11 @@
 'use strict';
 const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overly');
+const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+for( let i = 0 ; i < btnsOpenModal.length; i++){
+    btnsOpenModal[i].addEventListener('click',function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    })
+}
