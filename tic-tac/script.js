4f20864d1c2a6dfdea9e821ec7b313158e0dbbb9
Liton Islam <nurulislamliton24@gmail.com>
2025-02-06 23:15:30 +0600

    50% done

--- tic-tac/script.js
+++ tic-tac/script.js
@@ -1,5 +1,5 @@
 
-const cells = document.querySelector('.cell');
+const cells = document.querySelectorAll('.cell');
 const titleHeader = document.querySelector('#titleHeader');
 const oPlayerDisplay = document.querySelector('#oPlayerDisplay');
 const xPlayerDisplay = document.querySelector('#xPlayerDisplay');
@@ -11,4 +11,16 @@ let isGameStart = false;
 
 const inputCells = ['','','','','','','','',''];
 
-const winCondition =[[0,1,2],[3,4,5],[6,7,8],]
+const winCondition =[
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+];
+
+cells.forEach((cell,index) => {
+    cell.addEventListener('click',() => tapCell(cell,index))
+});
+
+function tapCell(cell,index){
+    console.log(cell);
+}
