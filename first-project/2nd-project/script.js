5ed058ee1425f5f06c19c0a044ad7d9983691f91
Liton Islam <litonislam.nitex@gmail.com>
2025-02-10 16:06:44 +0600

    2nd done

--- first-project/2nd-project/script.js
+++ first-project/2nd-project/script.js
@@ -2,10 +2,21 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+const btnsCloseModal = document.querySelector('.close-modal');
 
-for( let i = 0 ; i < btnsOpenModal.length; i++){
-    btnsOpenModal[i].addEventListener('click',function(){
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    })
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+  console.log(btnsOpenModal[i]);
 }
+
+overlay.addEventListener('click', closeModal);
+btnsCloseModal.addEventListener('click', closeModal);
