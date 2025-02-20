373f040de28455216c533f887440bae0ac822547
Liton <litonislam.nitex@gmail.com>
2025-02-20 15:31:11 +0600

    dice des ign done

---
+++ dice/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//selecting elements
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEl.classList.add('hidden');
