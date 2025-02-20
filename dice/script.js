c1f8ff34f1639fc3cf428574682b8d1d2d06a5b0
Liton <litonislam.nitex@gmail.com>
2025-02-20 17:46:28 +0600

    dice add

--- dice/script.js
+++ dice/script.js
@@ -5,7 +5,21 @@ const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+    //generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    console.log(dice);
+
+    //check for rolled 1 if true , switch to new player
+})
