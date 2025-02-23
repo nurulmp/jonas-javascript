e464ccccf7ce2c9ae6762e20b7ee567ca0cdeea9
Liton <litonislam.nitex@gmail.com>
2025-02-23 16:26:40 +0600

    cureent score and change player add

--- dice/script.js
+++ dice/script.js
@@ -1,8 +1,13 @@
 'use strict';
 
 //selecting elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +17,10 @@ score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 btnRoll.addEventListener('click', function () {
     //generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
@@ -19,7 +28,21 @@ btnRoll.addEventListener('click', function () {
     // display dice
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
-    console.log(dice);
+
 
     //check for rolled 1 if true , switch to new player
+
+    if (dice !== 1) {
+        //add dice to current score
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        //switch player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0EL.classList.toggle('player--active');
+        player1EL.classList.toggle('player--active');
+        console.log(activePlayer);
+    }
 })
