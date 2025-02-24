7042cf1b504c5762bd71a2c8fa23280f7221cb40
Liton <litonislam.nitex@gmail.com>
2025-02-24 17:56:13 +0600

    dice done

--- dice/script.js
+++ dice/script.js
@@ -20,29 +20,58 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+// switch player
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle('player--active');
+    player1EL.classList.toggle('player--active');
+};
 
 btnRoll.addEventListener('click', function () {
-    //generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
+    if (playing) {
+        //generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-    // display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+        // display dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
+
+        //check for rolled 1 if true , switch to new player
+
+        if (dice !== 1) {
+            //add dice to current score
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            //switch player
+            switchPlayer();
+        }
+    }
+})
+
+btnHold.addEventListener('click', function () {
+    if (playing) {
+        //add current score to active players score
+        scores[activePlayer] += currentScore;
+        //scores[1] = score[1]+currenScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
 
-    //check for rolled 1 if true , switch to new player
+        //check if players score is >= 100
+        //finish the game
+        if (scores[activePlayer] >= 20) {
+            playing = false;
+            diceEl.classList.add('hidden');
 
-    if (dice !== 1) {
-        //add dice to current score
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
-        //switch player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0EL.classList.toggle('player--active');
-        player1EL.classList.toggle('player--active');
-        console.log(activePlayer);
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player-active');
+        } else {
+            //switch to the next player
+            switchPlayer();
+        }
     }
 })
