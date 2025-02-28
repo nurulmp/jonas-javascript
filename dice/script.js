8fe917f0c94ad7c9494640ca6a4d80ffb874ae57
Liton <litonislam.nitex@gmail.com>
2025-02-28 18:02:41 +0600

    add new add

--- dice/script.js
+++ dice/script.js
@@ -13,15 +13,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0EL.textContent = 0;
-score1EL.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+    score0EL.textContent = 0;
+    score1EL.textContent = 0;
+    diceEl.classList.add('hidden');
 
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    score0EL.textContent = 0;
+    score1EL.textContent = 0;
+    current0EL.textContent = 0;
+    current1EL.textContent = 0;
+    player0EL.classList.remove('player--winner');
+    player1EL.classList.remove('player--winner');
+    player0EL.classList.add('player--active');
+    player1EL.classList.remove('player--active');
+};
+
+init();
 // switch player
 const switchPlayer = function () {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -63,7 +76,7 @@ btnHold.addEventListener('click', function () {
 
         //check if players score is >= 100
         //finish the game
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             playing = false;
             diceEl.classList.add('hidden');
 
@@ -75,3 +88,5 @@ btnHold.addEventListener('click', function () {
         }
     }
 })
+
+btnNew.addEventListener('click', init)
