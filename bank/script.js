fc1e66b34ffc5d2e584de107f0cdc23f78d1d2bc
Liton Islam <litonislam.nitex@gmail.com>
2025-06-18 17:58:40 +0600

    feat:transfer date add

--- bank/script.js
+++ bank/script.js
@@ -5,6 +5,19 @@ const account1 = {
   movements: [200, -400, 300, 500, -350, 1200],
   interestRate: 122,
   pin: 111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-07-26T17:01:17.194Z",
+    "2020-07-28T23:36:17.929Z",
+    "2020-08-01T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -12,38 +25,23 @@ const account2 = {
   movements: [500, 300, -200, 450, -100],
   interestRate: 100,
   pin: 222,
-};
 
-const account3 = {
-  owner: "Hasan Mahmud",
-  movements: [1000, -500, 700, 200, -300],
-  interestRate: 110,
-  pin: 333,
-};
-
-const account4 = {
-  owner: "Farhana Rahman",
-  movements: [250, -100, 400, -150, 600],
-  interestRate: 105,
-  pin: 444,
-};
-
-const account5 = {
-  owner: "Zahidul Islam",
-  movements: [800, -200, 300, -100, 500],
-  interestRate: 95,
-  pin: 555,
-};
-
-const account6 = {
-  owner: "Mitu Akter",
-  movements: [600, -300, 400, -250, 700],
-  interestRate: 115,
-  pin: 666,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 // Grouping all accounts in an array
-const accounts = [account1, account2, account3, account4, account5, account6];
+const accounts = [account1, account2];
 
 const labelWelcome = document.querySelector(".welcome");
 const labelDate = document.querySelector(".date");
@@ -70,15 +68,25 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovments = function (movements) {
+const displayMovments = function (acc, sort = false) {
   containerMovements.innerHTML = "";
-  movements.forEach(function (mov, i) {
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    const displayDate = `${day}/${month}/${year}`;
     const html = ` <div class="movements__row">
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__date">3 days ago</div>
+          <div class="movements__date">${displayDate}</div>
           <div class="movements__value">${mov}</div>
         </div>`;
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -124,7 +132,7 @@ createUsernames(accounts);
 
 const updateUi = function (acc) {
   //displau movements
-  displayMovments(acc.movements);
+  displayMovments(acc);
   //display blance
   calcPrintBlance(acc);
   //display summery
@@ -139,8 +147,8 @@ updateUi(currentAccount);
 containerApp.style.opacity = 100;
 
 const now = new Date();
-const day = now.getDate();
-const month = now.getMonth() + 1;
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
 const year = now.getFullYear();
 const hour = now.getHours();
 const min = now.getMinutes();
